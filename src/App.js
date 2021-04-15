f642b32b2473614ae62617a15aee68b15be27b0c
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2021-04-15 02:20:53 +0200

    Material UI demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import {Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,Toolbar,Container,Button} from '@material-ui/core'
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import useStyles from './style' ;
+const cards=[1,2,3,4,5,6,7,8,9];
+const App = () => {
+    const classes=useStyles() ;
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+               <Toolbar>
+                   <PhotoCameraIcon className={classes.icon}/>
+                   <Typography variant="h6">Photo Album</Typography>
+               </Toolbar>
+            </AppBar>
+            <main>
+               <div className={classes.container}>
+                   <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Hello everyone,this is a photo album and i 'am trying to make this sentence as long as possible so we can see how does it look like on the screen !
+                        </Typography>
+                        <div className={classes.button}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">See my photos</Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">Secondary actions</Button>
+                                </Grid>
+                            </Grid>
+                        </div> 
+                   </Container>
+               </div>
+               <Container className={classes.cardGrid} maxWidth="md">
+                   <Grid container spacing={4}>
+                       {
+                           cards.map((card)=>(
+                            <Grid item key={card} xs={12} sm={6} md={4} >
+                            <Card className={classes.card}>
+                                <CardMedia className={classes.cardMedia} image='https://source.unsplash.com/random' title="image title"/>
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        Heading
+                                    </Typography>
+                                    <Typography >
+                                        This is a media card ,You can use this section to describe this section !
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="primary">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                           ))
+                       }
+                       
+                   </Grid>
+               </Container>
+            </main>
+            <footer className={classes.footer}>
+                       <Typography variant="h6" align="center" gutterBottom>
+                           Footer
+                       </Typography>
+                       <Typography variant="subtitle1" align="center" color="textSecondary">
+                            Something here to give the footer a purpose
+                       </Typography>
+            </footer>
+        
+        </>
+    )
 }
 
-export default App;
+export default App
